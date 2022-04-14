0098f262d0a788045875c6f713d03e4acffe3696
Justin Li <justinlit23@gmail.com>
2022-04-14 15:36:41 -0700

    commit tree f4bffbeaa782d0752a2d55a29a27ad5964502a1c

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const submitData = (username, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            "name": username,
+            "email": email
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        const newUser = document.createElement("div")
+        newUser.textContent = data.id
+        document.body.append(newUser)
+    })
+    .catch(error => {
+        alert("Something went wrong.")
+        const errorMsg = document.createElement("div")
+        errorMsg.textContent = "Unauthorized Access"
+        document.body.append(errorMsg)
+        console.log("Something went wrong.")
+    } )
+}
